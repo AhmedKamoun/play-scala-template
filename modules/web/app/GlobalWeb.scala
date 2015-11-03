2915a703e8792340081cf73f9bf181eb4e0dbf38
Ahmed Kamoun <ahmedkamoun.insat@gmail.com>
2015-11-03 13:09:28 +0100

    change default server behavior to return only json response

--- modules/web/app/GlobalWeb.scala
+++ modules/web/app/GlobalWeb.scala
@@ -1,13 +1,19 @@
 import akka.actor.Props
 import controllers.web.actors.Register
+import exception.FailResult
+import exception.FailResultWrites._
 import org.springframework.context._
 import org.springframework.context.support._
 import play.api.Play.current
 import play.api.libs.concurrent.Akka
-import play.api.mvc.WithFilters
+import play.api.libs.json.Json
+import play.api.mvc.Results._
+import play.api.mvc.{RequestHeader, WithFilters}
 import play.api.{Application, GlobalSettings}
 import play.filters.headers.SecurityHeadersFilter
 import security.filters.CORSFilter
+
+import scala.concurrent.Future
 
 
 object GlobalWeb extends WithFilters(CORSFilter, SecurityHeadersFilter()) with GlobalSettings {
@@ -26,6 +32,28 @@ object GlobalWeb extends WithFilters(CORSFilter, SecurityHeadersFilter()) with G
   override def getControllerInstance[A](clazz: Class[A]): A = {
     ctx.getBean(clazz).asInstanceOf[A]
 
+  }
+
+  // 500 - internal server error
+  override def onError(request: RequestHeader, throwable: Throwable) = {
+    Future.successful(InternalServerError(
+      Json.toJson(FailResult("please contact the backend developers team for that: " + throwable.getMessage))
+    ))
+  }
+
+
+  // 404 - page not found error
+  override def onHandlerNotFound(request: RequestHeader) = {
+    Future.successful(NotFound(
+      Json.toJson(FailResult("action not found: " + request.method + " " + request.path))
+    ))
+  }
+
+  //The onBadRequest operation will be called if a route was found, but it was not possible to bind the request parameters
+  override def onBadRequest(request: RequestHeader, error: String) = {
+    Future.successful(BadRequest(
+      Json.toJson(FailResult(error))
+    ))
   }
 
 }
